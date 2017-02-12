9327135e426aa301e3938f934b68d62fc59913d6
Marie-Eve Dumas <marie@Marie-Eves-MacBook-Pro.local>
2017-02-12 15:32:20 -0500

    A binary number generator that output 2x1000 binary numbers and adds them.

---
+++ src/bin_generator.py
@@ -0,0 +1,34 @@
+from sys import argv
+import random as r
+
+# Create an input and output file
+input_file = open('input.txt','w')
+output_file = open('output.txt','w')
+
+# Convert a decimal to a binary string
+def dec2bin (x):
+ return str(bin(x)[2:])
+
+# Transform a binary string to a 32 char long string
+def bin2biggerbin(x):
+ return ('0' * (32 - len(x)) ) + x
+
+m = [0,0]
+
+for i in range(1000):
+  for j in range(2):
+    # Generate a 31 char long binary in the decimal format
+    number = r.getrandbits(31)
+    tmp = dec2bin(number)
+    tmp=bin2biggerbin(tmp)
+    m[j] = tmp
+    input_file.write('%s\t' % tmp)
+  # Add the 2 random binaries and write the result
+  result = dec2bin(int(m[0],2)+int(m[1],2))
+  output_file.write(bin2biggerbin(result)+'\n')
+  input_file.write('\n')
+
+# Close the files
+input_file.close()
+output_file.close()
+
