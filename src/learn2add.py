51b83c99218a3e8e8fc4948eee69d27a4299f4cd
Genevieve Bourgeois <genevievebourgeois@Genevieves-MacBook-Pro.local>
2017-06-01 19:33:29 -0400

    Added error plots

--- src/learn2add.py
+++ src/learn2add.py
@@ -3,7 +3,7 @@ import numpy as np
 import theano
 import lasagne
 import theano.tensor as T
-
+import matplotlib.pyplot as plt
 
 def data_splitter(array_data, array_ratios):
     size = array_data.shape
@@ -56,7 +56,7 @@ def creating_model(nb_bits_number, batch_size=1):
             updates=updates,
             allow_input_downcast=True)
 
-    return input_var, train_fn, l_out
+    return input_var, train_fn, l_out, l_hidden
 
 
 def get_sets(path_main_dataset_x, path_main_dataset_y):
@@ -71,6 +71,8 @@ def main(nb_epoch, path_main_dataset_x, path_main_dataset_y, batch_size=1):
     # Increasing the batch_size will help stabilize the gradient.  I've already
     # tried it without much success.
     batch_size = 1
+    x_value = []
+    y_value = []
 
     train_x, train_y, val_x, val_y, test_x, test_y = get_sets(
             path_main_dataset_x,
@@ -93,8 +95,10 @@ def main(nb_epoch, path_main_dataset_x, path_main_dataset_y, batch_size=1):
             nb_bits_number)
 
     # We create the layers for our neural network
-    input_var, train_fn, l_out = creating_model(nb_bits_number)
+    input_var, train_fn, l_out, l_hidden = creating_model(nb_bits_number)
 
+    plt.ion()
+    plt.figure(1)
     for epoch in range(nb_epoch):
         loss = 0
 
@@ -105,7 +109,7 @@ def main(nb_epoch, path_main_dataset_x, path_main_dataset_y, batch_size=1):
             train_fn_result = train_fn(np.array([input_batch]), target_batch)
             loss += train_fn_result
 
-        if epoch % 5 == 0:
+        if epoch % 1 == 0:
             # Every n epochs we will compute the error on the training and
             # valisation datasets
             print("Epoch %d: Loss %g" % (epoch, loss / len(train_x_batch)))
@@ -114,7 +118,9 @@ def main(nb_epoch, path_main_dataset_x, path_main_dataset_y, batch_size=1):
             test_prediction = lasagne.layers.get_output(
                     l_out,
                     deterministic=True)
-
+	    
+            print(test_prediction)
+	
             # We define a theano function that takes one inputs and returns the
             # precition for that input
             predict_fn = theano.function([input_var], test_prediction)
@@ -124,15 +130,28 @@ def main(nb_epoch, path_main_dataset_x, path_main_dataset_y, batch_size=1):
                 nb_errors.append(np.sum(np.absolute(train_y[j] - pred_bin)))
 
             print("Error on training set: %1.4f/%d ",
-                    (np.mean(nb_errors), nb_bits_number)
+                    (np.mean(nb_errors), nb_bits_number))
             # print("Error on validation set: %1.4f/%d ",
             #         (np.mean(nb_errors), nb_bits_number)
 
-            # TODO save model every n iterations.
+            # Plot mean error and save model every n iterations.
+            x_value.append(epoch)
+            y_value.append(np.mean(nb_errors))
+	    plt.scatter(epoch,np.mean(nb_errors),color='darkred')
+            plt.xlabel('Epoch')
+            plt.ylabel('Mean Error')
+            plt.title('Error as a function of the Epoch')
+            plt.pause(0.0001)
+            plt.draw()
 
+	    model_saved = lasagne.layers.get_all_param_values(l_out)
+	    #np.savetxt('ModelSave.txt', model_saved)
+
+	    # Then we need to use the set_all_param_values to load the model
+            print(model_saved)	
 
 if __name__ == "__main__":
     main(batch_size=1,
          path_main_dataset_x="../data/features.txt",
          path_main_dataset_y="../data/targets.txt",
-         nb_epoch=5000)
+         nb_epoch=3)
